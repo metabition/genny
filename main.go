26c7265250ac8c319629ab9ee01d790abefed019
Mat Ryer <thatmatryer@gmail.com>
2014-12-01 10:10:07 -0800

    fixed instructions

--- main.go
+++ main.go
@@ -10,15 +10,16 @@ func main() {
 
 Run this:
 
-  cd .. ; rm -r $GOPATH/src/github.com/metabition/genny ; rm $GOPATH/bin/genny ; go get https://github.com/cheekybits/genny
+  cd .. ; rm -r $GOPATH/src/github.com/metabition/genny ; rm $GOPATH/bin/genny ; go get github.com/cheekybits/genny
   
 Or do this:
 
   1. Delete $GOPATH/src/github.com/metabition/genny
   2. Delete $GOPATH/bin/genny
-  3. go get https://github.com/cheekybits/genny
+  3. go get github.com/cheekybits/genny
 
 Sorry, we won't move it again :)
     `)
 	os.Exit(1)
 }
+su
